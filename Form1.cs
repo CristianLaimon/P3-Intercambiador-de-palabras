7fc707fbb5f813db5c40d10290678daa1a7cc491
Kristan Ruíz <itan_07@hotmail.com>
2023-05-23 10:03:01 -0800

    Avances form1. Ahora se realiza una copia delarchivo, se trabaja en él
    - falta que muestre el contenido de este archivo

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices.ComTypes;
+
 namespace SesmaSantiago_RuizLimon_Practica3
 {
     public partial class Form1 : Form
@@ -6,6 +8,8 @@ namespace SesmaSantiago_RuizLimon_Practica3
         private StreamWriter escritor;
         private FileStream flujo;
         private StreamReader lector;
+        private string nombreArchivo, rutaArchivo, rutaProyecto;
+
 
         //private char[] textoChar, inputChar, outputChar;
         public Form1()
@@ -61,10 +65,17 @@ namespace SesmaSantiago_RuizLimon_Practica3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            if (!File.Exists(Path.Combine(Application.StartupPath, "Modificados")))
+            if (!File.Exists(Path.Combine(Application.StartupPath, "Modificados"))) //Se crean manualmente debido a que visual no permite copiar carpetas vacías, y estás lo estarán.
             {
                 Directory.CreateDirectory("Modificados");
             }
+
+            if (!File.Exists(Path.Combine(Application.StartupPath, "Temp")))
+            {
+                Directory.CreateDirectory(Path.Combine(Application.StartupPath, "temp"));
+            }
+
+
             openFileDialog1.InitialDirectory = Path.Combine(Application.StartupPath, "Originales");
             toolStripStatusLabel1.Text = "Hecho por: Diana Sesma Yulissa Santiago y Kristan Ruíz Limón";
             textBoxInput.Enabled = false;
@@ -73,23 +84,42 @@ namespace SesmaSantiago_RuizLimon_Practica3
 
         private void Reemplazar()
         {
-            string input = textBoxInput.Text, output = textBoxOutput.Text, texto = richTextBoxOriginal.Text;
-            string textoModificado = texto.Replace(input, output);
+            string rutaArchivoTemporal = Path.Combine(rutaProyecto, "temp", nombreArchivo);
+            string input = textBoxInput.Text;
+            string output = textBoxOutput.Text;
+            string texto = richTextBoxOriginal.Text;
+            string textoModificado = texto.Replace(input, output); //Se creo una copia y se modifico, falta sobreescribir el archivo con este nuevo contenido.
 
-            File.WriteAllText(openFileDialog1.FileName, textoModificado);
+            File.Copy(rutaArchivo, rutaArchivoTemporal, true); //Se realiza una copia del archivo original
 
-            richTextBoxModificado.Text = lector.ReadToEnd();
-            flujo.Seek(0, SeekOrigin.Begin);
+            File.WriteAllText(rutaArchivoTemporal, textoModificado); //Se sobreescribe el contenido del archivo temporal.
+
+            using (FileStream flujoTemporal = new FileStream(rutaArchivo, FileMode.Open, FileAccess.ReadWrite)) //Se muestra el contenido recien cambiado del archivo temporal usando otro filestream (el segundo) temporalemnte
+            {
+                StreamReader lectorTemporal = new StreamReader(flujoTemporal);
+                
+            }
+
+            //File.WriteAllText();
+
+
+
+            //richTextBoxModificado.Text = lector.ReadToEnd();
+            //flujo.Seek(0, SeekOrigin.Begin);
         }
 
         private void SeleccionarFile()
         {
+
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                labelNombreArchivoOriginal.Text = Path.GetFileName(openFileDialog1.FileName);
-                toolStripStatusLabel1.Text = openFileDialog1.FileName;
+                rutaArchivo = openFileDialog1.FileName;
+                nombreArchivo = Path.GetFileName(openFileDialog1.FileName);
+                rutaProyecto = Application.StartupPath;
+                labelNombreArchivoOriginal.Text = Path.GetFileName(rutaArchivo);
+                toolStripStatusLabel1.Text = rutaArchivo;
 
-                AbrirFlujo(openFileDialog1.FileName, FileMode.Open, FileAccess.ReadWrite);
+                AbrirFlujo(rutaArchivo, FileMode.Open, FileAccess.ReadWrite);
 
                 richTextBoxOriginal.Text = lector.ReadToEnd();
                 flujo.Seek(0, SeekOrigin.Begin);
